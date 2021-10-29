69a879a293648a8ba423ea4b622ea5b657d777ac
The Jabberwock <zazyzaya@gmail.com>
2021-10-28 22:42:23 -0400

    close to done

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ app.layout = html.Div(
     [     
         dcc.Store(id='memory'),
         dcc.Store(id='graph-cache'),
+        dcc.Store(id='last-forecast'),
         
         # Search bar
         html.Div([
@@ -140,22 +141,38 @@ app.layout = html.Div(
         # Footer tools
         html.Div(
             [
-                html.P('Derivative Delta Size: ', style={'display': 'inline-block'}),
-                dcc.Input(
-                    id='rolling-avg',
-                    value='4',
-                    type='number',
-                    style={
-                        'width': '40px',
-                        'background-color': 'gray',
-                        'color': 'white',
-                        'margin-left': '5px',
-                        'margin-right': '5px',
-                        'border-radius': '10px',
-                    },
-                    debounce=False
+                html.Div([
+                    html.P('Derivative Delta Size: ', style={'display': 'inline-block'}),
+                    dcc.Input(
+                        id='rolling-avg',
+                        value='4',
+                        type='number',
+                        style={
+                            'width': '40px',
+                            'background-color': 'gray',
+                            'color': 'white',
+                            'margin-left': '5px',
+                            'margin-right': '5px',
+                            'border-radius': '10px',
+                        },
+                        debounce=False
+                    ),
+                    html.P('weeks', style={'display': 'inline-block','margin-right': '10px'})
+                ],
+                style={'display': 'inline-block'}
                 ),
-                html.P('weeks', style={'display': 'inline-block'})
+                html.Div([
+                    html.P("Display forecast: ", style={'display': 'inline-block'}),
+                    dcc.RadioItems(
+                        id='forecast',
+                        options=[
+                            {'label': '', 'value': ''}
+                        ],
+                        style={'display': 'inline-block'},
+                        labelStyle={'display': 'inline-block'},
+                        value=''
+                    )
+                ])
             ],
             style={
                 'width': '100%',
@@ -173,51 +190,87 @@ app.layout = html.Div(
 @app.callback(
     [
         Output('live-graph', 'figure'),
-        Output('graph-cache', 'data')
+        Output('graph-cache', 'data'),
+        Output('last-forecast', 'data')
     ],
     [
         Input({'type': 'derivatives', 'index': ALL}, 'value'),
-        Input('rolling-avg', 'value')
+        Input('rolling-avg', 'value'),
+        Input('forecast', 'value')
     ],
     [
         State('live-graph', 'figure'),
         State('memory', 'data'),
-        State('graph-cache', 'data')
+        State('graph-cache', 'data'),
+        State('last-forecast', 'data')
     ],
     prevent_initial_call=True
 )
-def update_graph(_, smooth, figure, mem, cached):
+def update_graph(_, smooth, forecast, figure, mem, cached, last_forecast):
     ctx = dash.callback_context.triggered
     print('update_graph: ' + str(ctx))
     print(mem)
 
     smooth = int(smooth)
+    cached = {} if cached is None else cached
+    data = {} if not cached else cached['data']
+    last_forecast = {'data': ''} if not last_forecast else last_forecast
 
     if ctx[0]['prop_id'] == '.':
         figure['data'] = []
-        return figure, None 
+        figure['layout']['annotations'] = []
+        return figure, None, {'data': ''}
 
+    if ctx[0]['prop_id'] == 'forecast.value':
+        if not forecast:
+            figure['layout']['annotations'] = []
+            return {
+                'data': figure['data'],
+                'layout': figure['layout']
+            }, cached, {'data': ''}
+
+        fc = forecast
+        if fc not in data:
+            data[fc] = q.get_all(fc, smooth=smooth)
+            cached['data'] = data 
+
+        figure['layout']['annotations'] = data[fc][3]
+        return {
+            'data': figure['data'],
+            'layout': figure['layout']
+        }, cached, {'data': fc}
+        
+
+    # Update smoothing on derivatives
     if ctx[0]['prop_id'] == 'rolling-avg.value':
-        dat = []
+        fig_dat = []
+
+        to_update = {}
         for series in figure['data']:
+            print(series['name'])
             ticker, deriv = series['name'].split(':')
             
-            # Only recalculate derivatives
-            if deriv != '0':
-                pid = series['customdatasrc']
-                series = get_series(ticker, deriv, pid, smooth)
-                cached['data'][pid][deriv] = series 
+            if ticker not in to_update:
+                to_update[ticker] = []
+            
+            to_update[ticker].append(deriv)
+    
+        for ticker, derivs in to_update.items():
+            data[ticker][1] = q.first(*data[ticker][0], smooth)
+            data[ticker][2] = q.second(*data[ticker][1], smooth)
+            data[ticker][3] = q.find_zeros(*data[ticker][2])
 
-            dat.append(series)
-        
+            fig_dat += [
+                get_series(data[ticker][int(i)], ticker, i) 
+                for i in derivs if i < 3
+            ]
+
         return {
-            'data': dat,
+            'data': fig_dat,
             'layout': figure['layout']
-        }, cached
+        }, cached, last_forecast
             
 
-    cached = {} if cached is None else cached
-    data = {} if not cached else cached['data']
     pids = [str(json.loads(child['prop_id'].split('.')[0])['index']) for child in ctx]
 
     # Len is only greater than 1 if adding new stock
@@ -227,63 +280,74 @@ def update_graph(_, smooth, figure, mem, cached):
 
     # Adding a new or cached chart
     values = [str(v) for v in ctx[0]['value']]
-    pid = pids[0]
+    ticker = pids[0]
     
     # Avoid duplicates and remove deleted series
     new_dat = []
     for d in figure['data']:
         print(d['customdatasrc'])
-        if d['customdatasrc'] != pid and d['customdatasrc'] in mem['data']:
+        if d['customdatasrc'] != ticker and d['customdatasrc'] in mem['data']:
             new_dat.append(d)
 
     figure['data'] = new_dat
 
+    # Query yfinance if this is the first time seeing ticker
+    if ticker not in data:
+        data[ticker] = q.get_all(ticker, smooth=smooth)
+
     for v in values:
-        if pid not in data:
-            data[pid] = {}
-        if v not in data[pid]:
-            series = get_series(mem['data'][pid], v, pid, smooth)
-            
-            if series: 
-                data[pid][v] = series
-            else: 
-                print("Something went wrong; series %s, %s returned NULL" % (mem['data'][pid], v))
-        
-        figure['data'].append(data[pid][v])
+        if v != '3':
+            series = get_series(data[ticker][int(v)], ticker, v)
+            figure['data'].append(series)
     
     # Sync pid cache and graph cache
     cached_keys = list(data.keys())
-    for pid in cached_keys:
-        if pid not in mem['data']:
-            del data[pid]
+    for ticker in cached_keys:
+        if ticker not in mem['data']:
+            del data[ticker]
+            if ticker == last_forecast['data']:
+                figure['layout']['annotations'] = []
+                last_forecast = {'data': ''}
 
     cached['data'] = data
 
     return {
         'data': figure['data'], 
         'layout': figure['layout']
-    }, cached 
+    }, cached, last_forecast
 
-def get_series(ticker, deriv, pid, smooth):
-    if deriv == '0':
-        x,y = q.base(ticker)
-    elif deriv == '1': 
-        x,y = q.first(ticker, smooth=smooth)
-    elif deriv == '2':
-        x,y = q.second(ticker, smooth=smooth)
-    else:
-        return None
-    
+def get_series(xy, ticker, deriv):
+    #if deriv == '3':
+    #    fig['layout']['annotatons'] = xy
+
+    x,y = xy[0], xy[1]
     return go.Scatter(
         x=x, y=y, 
-        name=ticker + ':' + str(deriv), 
-        customdatasrc=pid,
+        name=ticker + ':' + deriv, 
+        customdatasrc=ticker,
         yaxis='y' if deriv=='0' else 'y2'
     )
 
 
 @app.callback(
-    Output('memory', 'data'),
+    Output('forecast', 'options'),
+    Input('memory', 'data'),
+    State('forecast', 'options')
+)
+def update_forecast_options(mem, options):
+    opts = [{'label': 'None', 'value': ''}]
+    for m in mem['data']:
+        opts.append(
+            {'label': m, 'value': m}
+        )
+    
+    return opts 
+
+@app.callback(
+    [
+        Output('memory', 'data'),
+        Output('search-text', 'value')
+    ],
     Input({'type': 'dynamic-delete', 'index': ALL}, 'n_clicks'),
     [
         State('memory', 'data'),
@@ -292,12 +356,14 @@ def get_series(ticker, deriv, pid, smooth):
 )
 def update_memory(_, mem, ticker):
     ctx = dash.callback_context.triggered
-    
+    print("update_memory: " + str(ctx))
+    print(ticker)
+
     # Final stock has been deleted
     if ctx[0]['prop_id'] == '.':
-        return {'data': {}}
+        return {'data': []}, ticker
 
-    mem = mem or {'data': {}}
+    mem = mem or {'data': []}
 
     # If the id is not in the dict, it's new. Otherwise, we're deleting it
     prop_ids = [str(json.loads(child['prop_id'].split('.')[0])['index']) for child in ctx]
@@ -305,18 +371,19 @@ def update_memory(_, mem, ticker):
 
     # Delete button was clicked
     if ctx[0]['value'] == 1:
-        del data[prop_ids[0]]
+        data.remove(prop_ids[0])
         mem['data'] = data 
-        return mem
+        return mem, ticker
 
     # New stock added
     for pid in prop_ids:
         if pid not in data:
-            data[pid] = ticker.upper()
+            data.append(ticker.upper())
             break 
     
     mem['data'] = data 
-    return mem
+    print("update_memory out: " + str(mem))
+    return mem, ''
 
 
 @app.callback(
@@ -324,30 +391,37 @@ def update_memory(_, mem, ticker):
     [
         Input('search-button', 'n_clicks'),
         Input({'type': 'dynamic-delete', 'index': ALL}, 'n_clicks'),
-        State('search-text', 'value')
+        Input('search-text', 'value')
     ],
-    State('stocks', 'children'),
+    [
+        State('stocks', 'children'),
+        State('memory', 'data')
+    ],
     prevent_initial_call=True
 )
-def add_or_del_security(idx, d, text, children):
+def add_or_del_security(_, d, text, children, mem):
     ctx = dash.callback_context.triggered[0]
     pid = ctx['prop_id']
     text = text.upper()
+    mem = mem or {'data': []}
+    mem = mem['data']
 
-    if pid == 'search-button.n_clicks' and text == '': 
-        return []
+    # Don't add blanks or dupes
+    if 'search' in pid:
+        if text == '' or text in mem: 
+            return children
 
     if children is None:
         children = [] 
 
     if 'search' in pid:
-        return add_new_stock(text, children, idx)
+        return add_new_stock(text, children)
 
     else:
-        idx = json.loads(pid.split('.')[0])['index']
-        return delete_stock(idx, children)
+        text = json.loads(ctx['prop_id'].split('.')[0])['index']
+        return delete_stock(text, children)
 
-def add_new_stock(ticker, component, idx):
+def add_new_stock(ticker, component):
     children = component or []
 
     new_div = html.Div([
@@ -356,7 +430,7 @@ def add_new_stock(ticker, component, idx):
             style={'text-align': 'center'}
         ),
         dcc.Checklist(
-                id={'type': 'derivatives', 'index': idx},
+                id={'type': 'derivatives', 'index': ticker},
                 options=[
                     {'label': 'Base Price\n', 'value': 0},
                     {'label': 'd/dx', 'value': 1},
@@ -367,10 +441,10 @@ def add_new_stock(ticker, component, idx):
         ), 
         html.Button(
             'Delete', 
-            id={'type': 'dynamic-delete', 'index': idx},
+            id={'type': 'dynamic-delete', 'index': ticker},
             style={'width': '100%', 'margin': 'auto'}
         )],
-        id={'type': 'dynamic-div', 'index': idx},
+        id={'type': 'dynamic-div', 'index': ticker},
         style={
             'width': '90%',
             'margin': 'auto',
@@ -382,11 +456,11 @@ def add_new_stock(ticker, component, idx):
     children.append(new_div)
     return children
 
-def delete_stock(d, children):
+def delete_stock(ticker, children):
     for i in range(len(children)):
         cn = children[i]['props']['id']['index']
 
-        if cn == d:
+        if cn == ticker:
             del children[i]
             break
 
--- queries.py
+++ queries.py
@@ -9,33 +9,73 @@ def get_hist(ticker, period):
     if not len(hist):
         return None 
 
-    return hist 
+    return hist
 
-def base(ticker, period='1y', smooth=0):
-    df = get_hist(ticker, period)['Close']
-    return df.index, df.values
+def base(ticker, period):
+    df = get_hist(ticker, period)
+    return df.index, df['Close'].values, df['Open'].values
 
-def first(ticker, base=None, period='1y', smooth=4):
-    hist = get_hist(ticker, period)
-    if hist is None:
-        return hist 
+def first(idx, close, open, smooth):
+    if not type(close) is np.ndarray:
+        close = np.array(close)
+        open = np.array(open) 
 
     offset = max(1, int(smooth*5))
-    open, close = hist['Open'].values, hist['Close'].values
     deriv = (close[offset:] - open[:-offset]) / open[:-offset]
 
-    return hist.index[offset:], deriv
+    return [idx[offset:], deriv]
 
-def second(ticker, der=None, period='1y', smooth=4):
-    if der is None:
-        index, der = first(ticker, period=period, smooth=smooth)
-    else:
-        index, der = der.index, der.values
+def second(idx, deriv, smooth):
+    if not type(deriv) is np.ndarray:
+        deriv = np.array(deriv)
 
-    # TODO only plot zeros of 2nd der
     offset = max(1, int(smooth*5))
-    return index[1:], der[offset:] - der[:-offset]
-    
+
+    return [idx[offset:], (deriv[offset:] - deriv[:-offset]) ]
+
+def find_zeros(idx, series, cutoff=0.05):
+    gt = series>0
+    zero_days = np.logical_xor(gt[1:], gt[:-1])
+
+    idx = idx[1:][zero_days]
+    magnitudes = series[1:][zero_days] - series[:-1][zero_days]
+
+    annotations = []
+    for i in range(idx.shape[0]):
+        mag = magnitudes[i]
+        
+        if abs(mag) < cutoff:
+            continue 
+        
+        annotations.append(
+            dict(
+                arrowcolor='green' if mag > 0 else 'red',
+                x=idx[i],
+                y=mag,
+                xref="x", yref="y2",
+                text="",
+                showarrow=True,
+                axref = "x", ayref='y2',
+                ax=idx[i],
+                ay=0,
+                arrowhead = 3,
+                arrowwidth=1.5
+            )
+        )
+
+    return annotations
+
+def get_all(ticker, period='1y', smooth=4):
+    idx, o, c = base(ticker, period)
+    d_idx, deriv = first(idx, c, o, smooth)
+    dd_idx, dderiv = second(d_idx, deriv, smooth)
+
+    return [
+        [idx, c, o],
+        [d_idx, deriv],
+        [dd_idx, dderiv],
+        find_zeros(dd_idx, dderiv)
+    ]
 
 def smoothing(x, N):
     if N == 0:
@@ -46,3 +86,6 @@ def smoothing(x, N):
 
 def dummy():
     return np.array([i for i in range(10)], dtype=float), np.random.rand((10))*10
+
+if __name__ == '__main__':
+    get_all('qqq')
